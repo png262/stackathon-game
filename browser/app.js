ad7efb3facc70bcd731709623a875b1b16442d67
Philip <png262@gmail.com>
2015-11-12 13:54:10 -0500

    got to original whiteboard state

--- browser/app.js
+++ browser/app.js
@@ -4,37 +4,22 @@ var ctx = canvas.getContext('2d');
 var button = document.querySelector("#save");
 
 socket.on('connect', function(){
-	socket.on("load", function(state){
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		var image = new Image();
-		image.src = state;
-		image.onload = function(){
-			ctx.drawImage(image, 0,0);
-		}
-	})
+	// socket.on("load", function(state){
+	// 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// 	var image = new Image();
+	// 	image.src = state;
+	// 	image.onload = function(){
+	// 		ctx.drawImage(image, 0,0);
+	// 	}
+	// })
+	console.log("I have connected to the server")
 
 	whiteboard.on("draw", function(start,end,color){
 		socket.emit("draw", start, end, color);
 
 	})
 
-	$("#save").on("click", function(){
-		var canvasState = canvas.toDataURL();
-		console.log(canvasState);
-		socket.emit('save', canvasState);
-	})
 
-	$("#kitchen").on("click", function(){
-		socket.emit("the-kitchen");
-	})
-
-	$("#grace-hopper").on("click", function(){
-		socket.emit("grace-hopper");
-	})
-
-	$("#turing").on("click", function(){
-		socket.emit("turing-hall");
-	})
 
 })
 
